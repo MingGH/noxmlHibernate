c1d8dd62172b89b46f501acfb8a9f24cb7ff1a05
MingGH <1558772318@qq.com>
2018-06-21 08:42:21 +0800

    在maven项目中，不知道怎么整合spring+hibernate4，所以暂定吧

---
+++ src/main/java/com/entity/Customer.java
@@ -0,0 +1,43 @@
+package com.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Integer customer_id;
+
+    private String first_name;
+
+    private String last_name;
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
